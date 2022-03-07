81fd14d9095a9c4daebaa19366c58e5a4b7a7791
gkzz <gakuji.tamaki@gmail.com>
2022-03-07 19:10:56 +0900

    mod

--- lambda-called-by-cloudwatch-log/lambda-cw-service/cwlog-slack-notify.js
+++ lambda-called-by-cloudwatch-log/lambda-cw-service/cwlog-slack-notify.js
@@ -1,12 +1,33 @@
 'use strict';
 
+const aws = require('aws-sdk')
+const zlib = require('zlib');
+
 module.exports.handler = async (event) => {
     console.log(JSON.stringify(event, null, 2));
+    /*
+    const promise = new Promise(function(resolve, reject) {
+        // https://stackoverflow.com/questions/62033425/cant-get-amazon-cloudwatch-logs-from-lambda-by-node-js
+        const cloudwatchlogs = new aws.CloudWatchLogs();
+        const params = {
+            logGroupName: '/aws/lambda/slack-notify'
+        }
+        cloudwatchlogs.describeLogStreams(params, function(err, data) {
+            if (err) console.log(err, err.stack); // an error occurred
+            else     console.log(data);           // successful response
+        });
+    })
+    */
+   // https://gist.github.com/aflansburg/132ef45a5ad9006469d8286bb99a0ec5
+    const payload     = Buffer.from(event.awslogs.data, 'base64');
+    const unzipeddata = zlib.unzipSync(payload);  
+    const logdata     = JSON.parse(unzipeddata.toString('utf8'));        
+    const logs        = JSON.stringify(logdata,null,2);
     return {
         statusCode: 200,
         body: JSON.stringify(
             {
-                message: 'Go Serverless v1.0! Your function executed successfully!',
+                message: logs,
                 input: event,
             },
             null,
